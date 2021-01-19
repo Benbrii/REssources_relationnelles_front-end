ffe9bdb4bc012b7834e92e68360feb53eae56450
Thibaut <thibaut.andre.delattre@gmail.com>
2021-01-19 13:35:41 +0100

    push modif

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import "./style.css";
 
+
 // components
 import ActuPage from "../../components/ActuPage";
+import NavBar from '../../components/NavBar'
 
 class Home extends Component {
   render() {
     return (
       <>
-        {// TOPNAVBAR DE THIBAUT
-        }
+          <NavBar/> 
         <h2 className="fil_title_center">Fil d'actualité</h2>
         <ActuPage />
       </>

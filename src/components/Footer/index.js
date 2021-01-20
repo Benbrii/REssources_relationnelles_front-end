34ab8f3f1c6dcbdf56a7d746df50595452ad74f7
Thibaut <thibaut.andre.delattre@gmail.com>
2021-01-20 19:02:31 +0100

    le footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import imgMinistere_interieur from '../../assets/img/Ministere_interieur.png'
+import imgHDF from '../../assets/img/region-hauts-de-france.svg'
+import imgcriseUp from '../../assets/img/crise-up.jpg'
+
+import './style.css'
+
+const Footer = () => (
+    <div className = "footer">
+        <footer className="page-footer blue text-center text-md-left mt-0">
+        <hr/>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h5 className="title mb-3">Nos soutient</h5>
+                        <div className="grind">
+                            <div className="blockSupport">
+                                <a href="https://www.interieur.gouv.fr"><img className="imgSupport" src={imgMinistere_interieur}/></a>
+                                <p>Le ministère de l'interieur</p>
+                            </div>
+                            <div className="blockSupport2">
+                                <a href="https://www.hautsdefrance.fr"><img className="imgSupport" src={imgHDF}/></a>
+                                <p>La région Haut-De-France</p>
+                            </div>
+                            <div className="blockSupport3">
+                                <a href="https://www.crise-up.com"><img className="imgSupport" src={imgcriseUp}/></a> 
+                                <p>Institut International de Gestion des Crises Humaines</p>                               
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <h5 className="title mb-3">Mention légal</h5>
+                        <ul>
+                            <li><a href="#">Données personnelles et cookies</a></li>
+                            <li><a href="#">Confidentialité</a></li>
+                            <li><a href="#">Condition général</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-copyright">
+                <div className="container-fluid">
+                    <p>REsource RElationnelles & CO © 2021</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+)
+export default Footer
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import img from '../../assets/img/CUBE.png'
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import './style.css'
 
 const NavBar = () => (
-    <div className = "header">
+    <div className = "headerNavBar">
         <nav className="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
             <a className="navbar-brand lienImage" href="#"><img src={img} alt="REsource RElationnelle"/></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">

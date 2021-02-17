3460ff81a26c4223751e69b127620be355c591e4
Benoît Briastre <benoit.briastre@gmail.com>
2021-02-17 12:06:00 +0100

    still trying serviceworker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./service-worker";
 
 // reactstrap
 import 'bootstrap/dist/css/bootstrap.min.css';

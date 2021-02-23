0070d695c886bdd4cd8940e6d4ad6138416a784e
Benoît Briastre <benoit.briastre@gmail.com>
2021-02-23 14:59:41 +0100

    button strap failed

--- src/components/connexion/index.js
+++ src/components/connexion/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { connectUser } from '../../actions/connexion.action';
 import './style.css';
-import Button from 'react-bootstrap/button'
+import { Button } from 'reactstrap';
 import Image from 'react-bootstrap/Image'
 import Modal from 'react-bootstrap/Modal'
 import Register from '../Register'
@@ -17,8 +17,8 @@ class Connexion extends Component {
                 email: null,
                 password: null
             },
-            modal:{
-                setShow:false
+            modal: {
+                setShow: false
             }
         };
         this.goConnect = this.goConnect.bind(this);
@@ -31,25 +31,25 @@ class Connexion extends Component {
         const { user } = this.state;
         this.props.connectUser(user).then(
             () => {
-                if(this.props.connexion === true){
+                if (this.props.connexion === true) {
                     window.location.href = "/home";
                 }
             }
         )
     }
 
-    handleShow(e){
-        this.setState({ modal: {setShow: true }})
-        console.log("SHOW",this.state.modal.setShow)
+    handleShow(e) {
+        this.setState({ modal: { setShow: true } })
+        console.log("SHOW", this.state.modal.setShow)
     }
 
-    handleClose(e){
-        this.setState({ modal: {setShow: false }})
+    handleClose(e) {
+        this.setState({ modal: { setShow: false } })
         console.log(this.state.modal.setShow)
     }
 
     render() {
-        console.log("CONNECTED:",this.props.connexion)
+        console.log("CONNECTED:", this.props.connexion)
 
         return (
             <div className="container connexionPage">
@@ -60,29 +60,29 @@ class Connexion extends Component {
 
                     <form className="form-horizontal col-md-6 connexionForm">
                         <h2>Connection</h2>
-                        
+
                         <div className="form-group">
                             <label htmlFor="email" className="col-sm-3 control-label">Email* </label>
                             <div className="col-sm-9">
                                 <input type="email" id="email" placeholder="Email" className="form-control" name="email"
-                                    onChange={e => { this.setState({ user: { ...this.state.user, email: e.target.value } }); e.preventDefault(); }}  />
+                                    onChange={e => { this.setState({ user: { ...this.state.user, email: e.target.value } }); e.preventDefault(); }} />
                             </div>
                         </div>
-                        
+
                         <div className="form-group">
                             <label htmlFor="password" className="col-sm-3 control-label">mot de passe*</label>
                             <div className="col-sm-9">
-                                <input type="password" id="password" placeholder="Password" className="form-control" 
+                                <input type="password" id="password" placeholder="Password" className="form-control"
                                     onChange={e => { this.setState({ user: { ...this.state.user, password: e.target.value } }); e.preventDefault(); }} />
                             </div>
                         </div>
                         <button type="submit" className="btn btn-primary btn-block col-sm-9 col-sm-offset-3" onClick={this.goConnect} >Sign in</button>
-                        <Button variant="success" className="col-sm-9 col-sm-offset-3" onClick={this.handleShow}>Sign up</Button>
+                        <Button color="success" className="col-sm-9 col-sm-offset-3" onClick={this.handleShow}>Sign up</Button>
                     </form>
                 </div>
-                
+
                 <Modal show={this.state.modal.setShow} onHide={this.handleClose}>
-                    <Register/>
+                    <Register />
                 </Modal>
             </div>
         );
@@ -90,16 +90,16 @@ class Connexion extends Component {
 }
 
 function mapStateToProps(state) {
-    
+
     return {
         connexion: state.connectReducer.connexion,
-        isLogged:state.connectReducer.isLogged
+        isLogged: state.connectReducer.isLogged
     };
 }
 
 
 function mapDispatchToProps(dispatch) {
-    
+
     return {
         connectUser: user => dispatch(connectUser(user))
     };

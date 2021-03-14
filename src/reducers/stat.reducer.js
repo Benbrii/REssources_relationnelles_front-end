d36c2e29ee5d9c938ffd51f038a5c7bab2303f8c
Benoît Briastre <benoit.briastre@gmail.com>
2021-03-14 12:39:50 +0100

    merging ok

--- src/reducers/stat.reducer.js
+++ src/reducers/stat.reducer.js
@@ -2,7 +2,7 @@ import * as statConst from "../const/stat.const";
 
 const initState = {
     dataConst: [],
-    dataCrea:[]
+    dataCrea: []
 };
 
 const statReducer = (state = initState, action) => {
@@ -11,39 +11,39 @@ const statReducer = (state = initState, action) => {
         // GET RESSOURCES
 
         case statConst.GET_STAT_FULFILLED:
-            
+
             const consArray = action.payload.data.consultStats
             const dataConsultation = []
 
-            for (let i = 0 ; i < 12; i++) {
+            for (let i = 0; i < 12; i++) {
                 for (let j = 0; j < consArray.length; j++) {
 
-                    if((i+1) == consArray[j].mois){
+                    if ((i + 1) === consArray[j].mois) {
                         dataConsultation[i] = parseInt(consArray[j].count, 10);
-                    }else{
+                    } else {
                         dataConsultation[i] = 0
                     }
                 }
             }
-            
+
             const creArray = action.payload.data.creaStat
             const dataCreation = []
 
-            for (let i = 0 ; i < 12; i++) {
+            for (let i = 0; i < 12; i++) {
                 for (let j = 0; j < creArray.length; j++) {
 
-                    if((i+1) == creArray[j].mois){
+                    if ((i + 1) === creArray[j].mois) {
                         dataCreation[i] = parseInt(creArray[j].count, 10);
-                    }else{
+                    } else {
                         dataCreation[i] = 0
                     }
                 }
             }
-            
+
             return {
                 ...state,
-                dataConst:dataConsultation,
-                dataCrea:dataCreation
+                dataConst: dataConsultation,
+                dataCrea: dataCreation
             };
 
         default:
